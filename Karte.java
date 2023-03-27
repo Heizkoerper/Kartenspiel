5e5899ff076f8c8138bdb41b99b68d73b1751487
Heizkoerper <navies.call.0j@icloud.com>
2023-03-27 14:11:04 +0200

    Initial commit

---
+++ Karte.java
@@ -0,0 +1,17 @@
+public class Karte {
+
+    private String farbe;
+    private String bild;
+    public void Karte(String farbe, String bild) {
+        this.farbe = farbe;
+        this.bild = bild;
+    }
+
+    public String getFarbe() {
+        return farbe;
+    }
+
+    public String getBild() {
+        return bild;
+    }
+}
---
+++ Kartenspiel.java
@@ -0,0 +1,11 @@
+public class Kartenspiel {
+
+    private Karte[] karten;
+    public void Kartenspiel() {
+        // Erstelle ein Array mit 32 Karten
+        karten = new Karte[32];
+
+        // Füge die Karten eines standard 32 Karten Kartenspiels hinzu
+
+    }
+}
